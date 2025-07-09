88fdecf29a3f0a0f2afb43c83ac77aea551af2ba
ArunAK4 <arunkumara619@gmail.com>
2025-07-09 23:46:21 +0530

    VPC

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "alb_security_group" {
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
