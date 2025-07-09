a39be3886e222333fe963174ae2a2aaf6665806c
ArunAK4 <arunkumara619@gmail.com>
2025-07-09 17:21:16 +0530

    syntax error resolved

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2"{
     ami = "ami-0d03cb826412c6b0f"
-    instance type = "t2.micro"
+    instance_type = "t2.micro"
     user_data = <<EOF
     
 #!/bin/bash
