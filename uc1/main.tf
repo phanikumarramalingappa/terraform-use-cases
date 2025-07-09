0bb73a62b3b05afa781db12c002cb3ad37fee0b5
ArunAK4 <arunkumara619@gmail.com>
2025-07-09 21:37:26 +0530

    VPC

--- uc1/main.tf
+++ uc1/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   source          = "../modules/vpc"
   vpc_name        = var.vpc_name
   vpc_cidr        = var.vpc_cidr
-  public_subnets  = [var.public_subnets]
-  private_subnets = [var.private_subnets]
-  azs             = [var.azs]
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  azs             = var.azs
 }
