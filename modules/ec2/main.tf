9da8c7bdf7d83458a33fcf33e693170240da9256
ArunAK4 <arunkumara619@gmail.com>
2025-07-09 21:52:47 +0530

    VPC

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -28,24 +28,3 @@ resource "aws_instance" "ec2-2"{
        Name = "Instance C"
      }
 }
-
-
-resource "aws_security_group" "security_group" {
-  name        = "uc1-sg"
-  description = "Allow HTTP traffic only from ALB"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
