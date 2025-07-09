0a06f5344d76ce630ac082321ce86a525b96ab8d
ArunAK4 <arunkumara619@gmail.com>
2025-07-09 21:16:25 +0530

    VPC

--- uc1/main.tf
+++ uc1/main.tf
@@ -3,3 +3,12 @@ module "ec2" {
   ami_id = var.ami_id
   vpc_id = "vpc-0253000a11759887b"
 }
+
+module "vpc" {
+  source          = "../modules/vpc"
+  name            = var.vpc_name
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = [var.public_subnets]
+  private_subnets = [var.private_subnets]
+  azs             = [var.azs]
+}
