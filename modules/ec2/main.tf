ffacbdfae0e437acb457e09c34ee489ef3ed1934
ArunAK4 <arunkumara619@gmail.com>
2025-07-09 22:36:45 +0530

    VPC

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2"{
     ami = var.ami_id
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     count = length(var.public_subnets)
     subnet_id = var.public_subnets[count.index]
     vpc_security_group_ids = [var.sg_id]
