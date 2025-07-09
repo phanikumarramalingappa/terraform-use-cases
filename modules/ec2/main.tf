9959ed54746e069545f15b2422fabebacd04b989
ArunAK4 <arunkumara619@gmail.com>
2025-07-09 22:30:52 +0530

    VPC

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -12,21 +12,3 @@ resource "aws_instance" "ec2"{
       Name = "Instance A"
     }
 }
-
-resource "aws_instance" "ec2-1"{
-    ami = var.ami_id
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [var.sg_id]
-    tags = {
-      Name = "Instance B"
-    }
-}
-
-resource "aws_instance" "ec2-2"{
-    ami = var.ami_id
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [var.sg_id]
-    tags = {
-       Name = "Instance C"
-     }
-}
