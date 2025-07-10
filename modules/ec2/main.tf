102deef92dd9db94cf85ec09dc5a08ce30160058
ArunAK4 <arunkumara619@gmail.com>
2025-07-10 08:00:07 +0530

    final

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ec2"{
     count = length(var.public_subnets)
     subnet_id = var.public_subnets[count.index]
     vpc_security_group_ids = [var.sg_id]
-    user_data = file(userdata-${count.index}.sh)
+    user_data = file("userdata-${count.index}.sh")
     tags = {
       Name = "Instance-${count.index}"
     }
