4a140d38aa8ca2656d2ebc7dda977ae806dfdd84
ArunAK4 <arunkumara619@gmail.com>
2025-07-10 19:35:19 +0530

    UC2

--- usecase-2/main.tf
+++ usecase-2/main.tf
@@ -1,3 +1,11 @@
+module "ec2" {
+  source         = "../modules/ec2"
+  ami_id         = var.ami_id
+  vpc_id         = module.vpc.vpc_id
+  sg_id          = module.security_group.sg_id
+  public_subnets = module.vpc.public_subnet_ids
+}
+
 module "vpc" {
   source          = "./module/vpc"
   vpc_name        = var.vpc_name
