feb73a62a982e7f0df4f4aa53c19371d48bd89c9
ArunAK4 <arunkumara619@gmail.com>
2025-07-10 20:27:50 +0530

    UC2

--- usecase-2/main.tf
+++ usecase-2/main.tf
@@ -22,19 +22,19 @@ module "vpc" {
 
 
 module "rds" {
-  source = "./module/rds"
-  db_identifier= "my-db-instance"
-  db_engine="postgres"
-  db_instance_class="db.t3.micro"
-  db_allocated_storage=5
-  db_username="admin"
-  db_password=var.db_password
-  db_subnet_group_name="my-db-subnet-group"
-  private_subnet_ids= module.vpc.private_subnet_ids
-  security_group_ids= module.security_group.rds_sg_id
-  multi_az=false
-  tags={
+source = "./module/rds"
+db_identifier= "my-db-instance"
+db_engine="postgres"
+db_instance_class="db.t3.micro"
+db_allocated_storage=5
+db_username="admin"
+db_password=var.db_password
+db_subnet_group_name="my-db-subnet-group"
+private_subnet_ids= module.vpc.private_subnet_ids
+security_group_ids= module.security_group.rds_sg_id
+multi_az=false
+tags={
   Environment = "test"
   Project = "my-project"
-  }
+  }
 }
