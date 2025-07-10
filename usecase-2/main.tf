7bf7b1611eda14cac199db865be595a0251b40f8
ArunAK4 <arunkumara619@gmail.com>
2025-07-10 18:12:49 +0530

    UC2

---
+++ usecase-2/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source          = "../modules/vpc"
+  vpc_name        = var.vpc_name
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  azs             = var.azs
+}
