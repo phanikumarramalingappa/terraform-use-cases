8a89a5fbecd506322e8caeb9b533097701686164
ArunAK4 <arunkumara619@gmail.com>
2025-07-10 19:40:21 +0530

    UC2

--- usecase-2/main.tf
+++ usecase-2/main.tf
@@ -6,6 +6,11 @@ module "ec2" {
   public_subnets = module.vpc.public_subnet_ids
 }
 
+module "security_group" {
+  source = "./module/security-group"
+  vpc_id = module.vpc.vpc_id
+}
+
 module "vpc" {
   source          = "./module/vpc"
   vpc_name        = var.vpc_name
---
+++ usecase-2/module/security-group/main.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "security_group" {
+  name        = "uc2-sg"
+  description = "Allow HTTP traffic only from ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
