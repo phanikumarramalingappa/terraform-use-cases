dcdb15cf477e2fd4bdae2d797a40288a6072971c
ArunAK4 <arunkumara619@gmail.com>
2025-07-11 14:52:55 +0530

    uc3 yml file

--- usecase-3/modules/ec2/main.tf
+++ usecase-3/modules/ec2/main.tf
@@ -5,12 +5,11 @@ provider "aws" {
 resource "aws_instance" "ec2"{
     ami = var.ami_id
     instance_type = "t2.medium"
-    count = length(var.public_subnets)
-    subnet_id = var.public_subnets[count.index]
+    subnet_id = var.public_subnets[0]
     vpc_security_group_ids = [var.sg_id]
     associate_public_ip_address = true
     user_data = file("${path.module}/userdata-0.sh")
     tags = {
-      Name = "Instance-${count.index}"
+      Name = "Instance-0"
     }
 }
--- usecase-3/modules/ec2/output.tf
+++ usecase-3/modules/ec2/output.tf
@@ -1,3 +1,3 @@
 output "instance_ids" {
-    value = [for instance in aws_instance.ec2 : instance.id]
+    value =aws_instance.ec2.ec2.id
 }
