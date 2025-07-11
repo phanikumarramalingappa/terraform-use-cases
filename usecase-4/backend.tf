c40a00fe54c7ee1a5f7ec9fb8e4ffa6feea118fd
ArunAK4 <arunkumara619@gmail.com>
2025-07-11 18:19:11 +0530

    uc4

--- usecase-4/backend.tf
+++ usecase-4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-usecases-batch6"
-    key          = "usecase3/terraform.tfstate"
+    key          = "usecase4/terraform.tfstate"
     region       = "eu-west-1"
     use_lockfile = true
   }
