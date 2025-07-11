68b2df7145312552c3d4436f697a879eeef807c4
ArunAK4 <arunkumara619@gmail.com>
2025-07-11 14:47:23 +0530

    uc3 yml file

--- usecase-3/modules/ec2/main.tf
+++ usecase-3/modules/ec2/main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "ec2"{
     ami = var.ami_id
     instance_type = "t2.medium"
-    count = length(var.public_subnets)
     subnet_id = var.public_subnets[0]
     vpc_security_group_ids = [var.sg_id]
     associate_public_ip_address = true
