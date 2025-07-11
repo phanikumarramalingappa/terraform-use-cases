29d49d6f8e0eb405d010448c003ad80fa2d7e9d3
ArunAK4 <arunkumara619@gmail.com>
2025-07-11 13:05:44 +0530

    usecase3

---
+++ usecase-3/variable.tf
@@ -0,0 +1,31 @@
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "Custom VPC cidr block"
+  type        = string
+}
+
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "vpc_name" {
+  description = "VPC name"
+  type        = string
+}
