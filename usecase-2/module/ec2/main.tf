d3ada4af81405507113ecf78021d86b68437da7f
ArunAK4 <arunkumara619@gmail.com>
2025-07-11 07:05:09 +0530

    UC2-u

--- usecase-2/module/ec2/main.tf
+++ usecase-2/module/ec2/main.tf
@@ -9,7 +9,6 @@ resource "aws_instance" "ec2" {
   subnet_id                   = var.public_subnets[count.index]
   vpc_security_group_ids      = [var.sg_id]
   associate_public_ip_address = true
-  user_data                   = file("${path.module}/userdata-${count.index}.sh")
   tags = {
     Name = "Instance-${count.index}"
   }
