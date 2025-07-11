11be520881fbdeb2467af718395a96f191df9299
ArunAK4 <arunkumara619@gmail.com>
2025-07-11 07:02:05 +0530

    UC2-u

--- usecase-2/module/ec2/variable.tf
+++ usecase-2/module/ec2/variable.tf
@@ -1,4 +1,19 @@
-variable "region" {
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
   type        = string
-  description = "AWS Region"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "sg_id" {
+  description = "Security group ID"
+  type = string
+}
+
+variable "public_subnets" {
+  description = "Public subnet ID's "
+  type = list(any)
 }
