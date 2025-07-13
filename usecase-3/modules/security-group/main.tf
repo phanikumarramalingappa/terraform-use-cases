ddb89ec5c0c00c16afd48576c37f3e7365e1b541
ArunAK4 <arunkumara619@gmail.com>
2025-07-13 14:22:44 +0530

    UC 3

--- usecase-3/modules/security-group/main.tf
+++ usecase-3/modules/security-group/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "security_group" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = aws_security_group.alb_security_group.id
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
