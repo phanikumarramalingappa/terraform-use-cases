fbfd1e6e04f7b7ff69a4cffa142cb1fd082ca740
ArunAK4 <arunkumara619@gmail.com>
2025-07-13 13:42:30 +0530

    s3 bucket name updated

--- hands-on/backend.tf
+++ hands-on/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-usecases-batch6"
-    key          = "handson-1/terraform.tfstate"
+    key          = "hands-on-1/terraform.tfstate"
     region       = "eu-west-1"
     use_lockfile = true
   }
