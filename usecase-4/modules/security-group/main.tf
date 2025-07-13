a01638dbd8d306279c63283d23ba13720f746049
ArunAK4 <arunkumara619@gmail.com>
2025-07-13 15:56:06 +0530

    UC 4

--- usecase-4/modules/security-group/main.tf
+++ usecase-4/modules/security-group/main.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+   ingress {
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
