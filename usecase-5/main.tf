cf9bc7f47fc6933ddbbff11e12a09a06cb4f1e69
ArunAK4 <arunkumara619@gmail.com>
2025-07-14 12:39:19 +0530

    initial usecase5 commit

---
+++ usecase-5/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "my_lambda_function"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"  # Adjust based on your code
+  runtime       = "python3.9"      # Or nodejs18.x, etc.
+
+  filename         = "${path.module}/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+
+  timeout = 10
+  memory_size = 128
+}
