7871e7125cb2d8c0ff29720e90f4c1b85f6bb74a
ArunAK4 <arunkumara619@gmail.com>
2025-07-14 09:58:47 +0530

    uc4

--- usecase-4/modules/ec2/userdata-0.sh
+++ usecase-4/modules/ec2/userdata-0.sh
@@ -10,10 +10,9 @@ systemctl enable docker
 mkdir -p ~/.docker/cli-plugins
 curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-git clone https://github.com/apache/incubator-devlake.git
-cd incubator-devlake
-cd backend
-cd ..
+git clone https://github.com/merico-dev/lake.git devlake-setup
+cd devlake-setup
+cp -arp devops/releases/lake-v0.21.0/docker-compose.yml ./
 cp env.example .env
 echo "ENCRYPTION_SECRET=password123" >> .env
 mv docker-compose-dev.yml compose.yml
