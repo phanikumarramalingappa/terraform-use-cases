1a0f2d47395daf601770ec4bede12400de398d08
ArunAK4 <arunkumara619@gmail.com>
2025-07-14 19:12:58 +0530

    uc5-sns

--- usecase-5/variable.tf
+++ usecase-5/variable.tf
@@ -2,3 +2,8 @@ variable "email" {
   type        = string
   description = "Enamil ID which is need to be subscribed"
 }
+
+variable "sns_topic_name" {
+    type = string
+    description = "SNS Topic name"
+}
