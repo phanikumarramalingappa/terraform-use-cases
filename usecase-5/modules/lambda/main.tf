c585a7fcf0bbd026bff1fa727dd5b2ba02f6e42d
ArunAK4 <arunkumara619@gmail.com>
2025-07-14 20:25:25 +0530

    uc5-lambda

--- usecase-5/modules/lambda/main.tf
+++ usecase-5/modules/lambda/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "lambda" {
   s3_bucket   = var.lambda_function.s3_bucket
   s3_key      = var.lambda_function.s3_key
-  name        = var.lambda_function.name
+  function_name        = var.lambda_function.name
   role        = var.lambda_function.role
   handler     = "lambda.lambda_handler"
   runtime     = "python3.9"
