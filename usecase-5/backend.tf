67f987e89b2f8519e98a850f029a4cf7e71a6a4c
ArunAK4 <arunkumara619@gmail.com>
2025-07-14 12:40:43 +0530

    uc5

---
+++ usecase-5/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket       = "terraform-usecases-batch6"
+    key          = "usecase5/terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
+  }
+}
