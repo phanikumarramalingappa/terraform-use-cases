97448576de2fba53e32e896629f1c3f7d44b5169
ArunAK4 <arunkumara619@gmail.com>
2025-07-15 21:11:10 +0530

    uc 6

--- usecase-6/modules/event/main.tf
+++ usecase-6/modules/event/main.tf
@@ -4,8 +4,8 @@ resource "aws_cloudwatch_event_rule" "schedule" {
     description = var.description
 }
 
-resource "aws_cloudwatch_event_rule" "schedule" {
-    name = var.name
-    schedule_expression = var.schedule_expression
-    description = var.description
+resource "aws_cloudwatch_event_target" "lambda" {
+    rule = aws_cloudwatch_event_rule.schedule.name
+    target_id = var.start_lambda_name
+    arn = var.start_lambda_arn
 }
