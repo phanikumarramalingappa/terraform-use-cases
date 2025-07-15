ff1cba8eda79b723e7effba61f754ea00419d76d
ArunAK4 <arunkumara619@gmail.com>
2025-07-15 21:17:55 +0530

    uc 6

--- usecase-6/modules/lambda/output.tf
+++ usecase-6/modules/lambda/output.tf
@@ -1,15 +1,15 @@
 output "start_lambda_function_arn" {
-    value = aws_lambda_function.start_lambda.lambda_arn
+    value = aws_lambda_function.start_lambda.arn   
 }
 
 output "start_lambda_function_name" {
-    value = aws_lambda_function.start_lambda.lambda_name
+    value = aws_lambda_function.start_lambda.function_name   
 }
 
 output "stop_lambda_function_arn" {
-    value = aws_lambda_function.stop_lambda.lambda_arn
+    value = aws_lambda_function.stop_lambda.arn   
 }
 
 output "stop_lambda_function_name" {
-    value = aws_lambda_function.stop_lambda.lambda_name
+    value = aws_lambda_function.stop_lambda.function_name   
 }
