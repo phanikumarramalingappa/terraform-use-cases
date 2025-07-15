30c154eb57d62ec1514240f4217ea6d9efff849a
ArunAK4 <arunkumara619@gmail.com>
2025-07-15 20:17:17 +0530

    uc 6

--- usecase-6/main.tf
+++ usecase-6/main.tf
@@ -4,6 +4,7 @@ module "lambda" {
   lambda_function = {
     start_name = "start-lambda"
     stop_name = "stop-lambda"
+    role = var.lambda_role
   }
   instance_id     = var.instance_id
   region          = var.region
