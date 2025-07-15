6288b2b2197e5de416a8639e3405a77e04185266
ArunAK4 <arunkumara619@gmail.com>
2025-07-15 19:42:19 +0530

    uc 6

--- usecase-6/modules/lambda/main.tf
+++ usecase-6/modules/lambda/main.tf
@@ -1,3 +1,9 @@
+data "archive_file" "zip" {
+  type = zip
+  source_file = "${path.module}/start_lambda.py"
+  output_path = "${path.module}/function.zip"
+}
+
 resource "aws_lambda_function" "lambda" {
   function_name        = var.lambda_function.name
   role        = var.lambda_function.role
@@ -6,6 +12,7 @@ resource "aws_lambda_function" "lambda" {
   memory_size = 128
   timeout     = 30
   publish     = true
+  filename = data.archive_file.zip.output_path
   environment {
     variables = {
       instance_id = var.instance_id
