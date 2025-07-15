c5f0f2bde4abd2d202384ac0781a30faf271693a
ArunAK4 <arunkumara619@gmail.com>
2025-07-15 19:37:54 +0530

    uc 6

--- usecase-6/modules/lambda/main.tf
+++ usecase-6/modules/lambda/main.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "lambda" {
   memory_size = 128
   timeout     = 30
   publish     = true
-  environment = {
+  environment {
     variables = {
       instance_id = var.instance_id
       region      = var.region
