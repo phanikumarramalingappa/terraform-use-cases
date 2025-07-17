9d3f3b7d75fca7db92f09336c286dcf3ccbe945b
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-17 08:11:17 +0530

    Update backend.tf

--- usecase-6/backend.tf
+++ usecase-6/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=1.0.0"
   backend "s3" {
-    bucket       = "terraform-usecases-batch6"
+    bucket       = "terraform-usecases-nsh-batch6"
     key          = "usecase6/terraform.tfstate"
     region       = "eu-west-1"
     use_lockfile = true
