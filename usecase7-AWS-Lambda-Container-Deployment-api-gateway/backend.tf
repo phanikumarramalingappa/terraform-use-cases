025bbeb4e6c8a950bcc2fd29a614ae88a1b83ead
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-17 23:51:31 +0530

    Update backend.tf

--- usecase7-AWS-Lambda-Container-Deployment-api-gateway/backend.tf
+++ usecase7-AWS-Lambda-Container-Deployment-api-gateway/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=1.0.0"
   backend "s3" {
     bucket       = "terraform-usecases-nsh-batch6"
-    key          = "usecase6/terraform.tfstate"
+    key          = "usecase-7/terraform.tfstate"
     region       = "ap-southeast-2"
     use_lockfile = true
   }
