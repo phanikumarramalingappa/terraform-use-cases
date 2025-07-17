bacfe642f12c767943673fd13964f68acaff1bbe
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-18 00:05:59 +0530

    Update iam.tf

--- usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/lambda/iam.tf
+++ usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/lambda/iam.tf
@@ -1,6 +1,6 @@
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
+  name = "lambda-exec-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
