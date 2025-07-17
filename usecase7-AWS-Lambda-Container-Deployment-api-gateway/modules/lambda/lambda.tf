bc7e00e506ab79c9e8fdc09573ab9b44f74ab79b
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-18 00:10:45 +0530

    Update lambda.tf

--- usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/lambda/lambda.tf
+++ usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/lambda/lambda.tf
@@ -6,11 +6,45 @@ resource "aws_lambda_function" "this" {
   image_uri     = var.lambda_image_uri
   timeout       = 10
   memory_size   = 128
+
   vpc_config {
     subnet_ids         = var.subnet_ids
     security_group_ids = var.security_group_ids
   }
-  depends_on = [aws_iam_role.lambda_exec]
+
+  depends_on = [aws_iam_role_policy_attachment.lambda_ecr_access]
+}
+
+resource "aws_iam_policy" "ecr_access" {
+  name        = "LambdaECRAccessPolicy"
+  description = "Allow Lambda to pull images from ECR"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ecr_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.ecr_access.arn
 }
 
 output "lambda_function_name" {
