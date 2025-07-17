0bfcb7c122abe05ab005b16e97e583a1e59953e7
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-17 20:40:51 +0530

    Create variables.tf

---
+++ usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/api_gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "lambda_arn" {
+  description = "Name to be used on lambda arn"
+  type        = string
+}
+
+variable "lambda_function_name" {
+    description = "Name to be used on lambda name"
+    type = string
+}
