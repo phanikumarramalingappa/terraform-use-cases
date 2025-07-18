9edc6f082e07cad282cd81fb5d04f61be33b19ca
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-18 13:26:29 +0530

    Update api_gateway.tf

--- usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/api_gateway/api_gateway.tf
+++ usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/api_gateway/api_gateway.tf
@@ -26,9 +26,14 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 }
 
 resource "aws_api_gateway_deployment" "rest_deployment" {
-  depends_on = [aws_api_gateway_integration.lambda_integration]
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  stage_name  = var.stage_name
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  depends_on  = [aws_api_gateway_integration.integration]
+}
+
+resource "aws_api_gateway_stage" "api_stage" {
+  stage_name    = var.stage_name
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.rest_deployment.id
 }
 
 resource "aws_lambda_permission" "api_gw" {
