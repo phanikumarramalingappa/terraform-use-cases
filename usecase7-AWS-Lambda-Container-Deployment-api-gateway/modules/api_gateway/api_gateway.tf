0d81a25cfafbd9f97a3806d342df577d94b9610f
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-18 16:22:25 +0530

    Update api_gateway.tf

--- usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/api_gateway/api_gateway.tf
+++ usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/api_gateway/api_gateway.tf
@@ -22,7 +22,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   http_method             = aws_api_gateway_method.proxy_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = var.lambda_arn
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
 }
 
 resource "aws_api_gateway_deployment" "rest_deployment" {
