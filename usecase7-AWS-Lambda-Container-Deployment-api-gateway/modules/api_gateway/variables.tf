3f58c7d1d8e65c75aec0ddabe0425f715dbc98cb
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-18 14:32:12 +0530

    Update variables.tf

--- usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/api_gateway/variables.tf
+++ usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/api_gateway/variables.tf
@@ -11,3 +11,8 @@ variable "stage_name" {
     description = "Name to be used on lambda name"
     type = string
 }
+variable "region" {
+  description = "AWS region where the API Gateway is deployed"
+  type        = string
+}
+
