ec4aa37047e91976e124a6189e3065f72073d705
phanikumarramalingappa <phaniramalingappa@gmail.com>
2025-07-18 13:04:24 +0530

    Update api_gateway.tf

--- usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/api_gateway/api_gateway.tf
+++ usecase7-AWS-Lambda-Container-Deployment-api-gateway/modules/api_gateway/api_gateway.tf
@@ -1,27 +1,34 @@
-
-resource "aws_apigatewayv2_api" "http_api" {
-  name          = "http-api"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name        = "rest-api"
+  description = "REST API for Lambda integration"
 }
 
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.http_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = var.lambda_arn
-  integration_method = "POST"
-  payload_format_version = "2.0"
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "{proxy+}"
 }
 
-resource "aws_apigatewayv2_route" "default" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_arn
+}
+
+resource "aws_api_gateway_deployment" "rest_deployment" {
+  depends_on = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = "prod"
 }
 
 resource "aws_lambda_permission" "api_gw" {
@@ -29,10 +36,9 @@ resource "aws_lambda_permission" "api_gw" {
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
 }
 
-
 output "api_endpoint" {
-  value = aws_apigatewayv2_api.http_api.api_endpoint
+  value = "${aws_api_gateway_deployment.rest_deployment.invoke_url}"
 }
