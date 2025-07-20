3cda605800131096a9d713e05a4c333909a00b45
EC2 Default User <ec2-user@ip-172-31-93-50.ec2.internal>
2025-07-20 15:07:21 +0000

    initial commit for usecase5

--- usecase-5/backend.tf
+++ usecase-5/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">=1.0.0"
   backend "s3" {
-    bucket       = "terraform-usecases-batch6"
+    bucket       = "terraform-usecases-nsh-batch-6"
     key          = "usecase5/terraform.tfstate"
-    region       = "eu-west-1"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
